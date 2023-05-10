4ead8fb8869d0a04118d42650111153807988547
Mateo1O <compla1@hotmail.com>
2023-05-10 01:51:17 +0000

    push function test

--- functions.c
+++ functions.c
@@ -1,18 +1,18 @@
+#include "monty.h"
 void _push(stack_t **stack, unsigned int line_number)
 {
-
-}
-
-void _pall(stack_t **stack, unsigned int line_number)
-{
-
-}
-
-void _pint(stack_t **stack, unsigned int line_number)
-{
-
-}
-void _pop(stack_t **stack, unsigned int line_number)
-{
-
+    stack_t *line = malloc(sizeof(stack_t));
+    (void)line_number;
+    
+    if (!line)
+    {
+        fprint(stderr, "error que no me acuerdo");
+        free(line);
+        return (-1);
+    }
+    if(*stack)
+        (*stack)->prev = line;
+    line->n = 0;
+    line->next = *stack;
+    *stack = line;
 }
