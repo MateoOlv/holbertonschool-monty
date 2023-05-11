f7ea27f31487245555c37f8624b29629016140ea
Mateo1O <compla1@hotmail.com>
2023-05-11 03:19:00 +0000

    a

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 
     if(argc != 2)
     {
-        fprintf(stderr, "USAGE: monty file");
+        fprintf(stderr, "USAGE: monty file\n");
         return (EXIT_FAILURE);
     }
     arch = fopen(argv[1], "r");
