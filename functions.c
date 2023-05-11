cf28f40454eadf728a6b20af34f08958eeeefb8f
Mateo1O <compla1@hotmail.com>
2023-05-11 17:42:39 +0000

    pop

--- functions.c
+++ functions.c
@@ -10,7 +10,7 @@ void _push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if (!token || !_digit(token)) {
+	if (!token || _digit(token) == 0 || !stack) {
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
@@ -28,20 +28,52 @@ void _pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *line = *stack;
 	(void)line_number;
-	
+	if (!(*stack))
+		return;
+	while (line->next)
+	{
+		line = line->next;
+	}
 	while (line)
 	{
 		printf("%d\n", line->n);
-		line = line->next;
+		line = line->prev;
 	}
 }
 
 void _pint(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp = *stack;
+
 	if (!(*stack))
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	printf("%d\n", tmp->n);
+}
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (tmp->next == NULL)
+	{
+		free(tmp);
+		*stack = NULL;
+	}
+	tmp = (*stack)->next;
+		free(*stack);
+	*stack = tmp;
+	if (!(*stack))
+		return;
+	(*stack)->prev = NULL;
 }
