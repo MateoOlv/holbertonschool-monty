7921cfc21ea8deb0d1db10ba1c758e1478e0f036
MateoOlv <6304@holbertonstudents.com>
2023-05-12 12:54:10 +0000

    Aux

--- auxfunctions.c
+++ auxfunctions.c
@@ -11,15 +11,12 @@ int _digit(char string[])
 {
 	int i;
 
-	if (!string)
-		return (0);
+	if (string[0] == '-')
+		string[0] = '0';
 	for (i = 0; string[i]; i++)
 	{
-		if (string[0] == '-')
-			string[0] = '0';
 		if (isdigit(string[i]) == 0)
 			return (0);
-		i++;
 	}
 	return (1);
 }
